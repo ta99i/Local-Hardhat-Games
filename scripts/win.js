64b437f45924d752085b632e121836fd86e12f1e
ta99i <abdesselamtaki@icloud.com>
2023-01-05 18:25:38 +0100

    modified:   scripts/deploy.js 	modified:   scripts/win.js

--- scripts/win.js
+++ scripts/win.js
@@ -1,23 +1,33 @@
 // add the game address here and update the contract name if necessary
-const gameAddr = "";
-const contractName = "Game1";
+const gameAddr = "0x610178da211fef7d417bc0e6fed39f05609ad788";
+const contractName = "Game5";
 
 async function main() {
-    // attach to the game
-    const game = await hre.ethers.getContractAt(contractName, gameAddr);
+  // attach to the game
+  const game = await hre.ethers.getContractAt(contractName, gameAddr);
 
-    // do whatever you need to do to win the game here:
-    const tx = await game.win();
+  // do whatever you need to do to win the game here:
+  //Game2
+  /*await game.setX(25);
+  await game.setY(25);
+  */
+  //game4
+  /*const tx = await game.win(56);*/
+  //game55
+  await game.giveMeAllowance(10001);
+  await game.mint(10001);
+  const tx = await game.win();
 
-    // did you win? Check the transaction receipt!
-    // if you did, it will be in both the logs and events array
-    const receipt = await tx.wait();
-    console.log(receipt);
+  // did you win? Check the transaction receipt!
+  // if you did, it will be in both the logs and events array
+
+  const receipt = await tx.wait();
+  console.log(receipt);
 }
 
 main()
   .then(() => process.exit(0))
-  .catch(error => {
+  .catch((error) => {
     console.error(error);
     process.exit(1);
   });
